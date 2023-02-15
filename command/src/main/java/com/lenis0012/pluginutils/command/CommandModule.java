3df3cbc9d3cbcab8e6b14d617aeeea6d6abc2061
Lennart ten Wolde <lenny.devmail@gmail.com>
2023-02-15 21:35:09 +0100

    fix: compile error

--- command/src/main/java/com/lenis0012/pluginutils/command/CommandModule.java
+++ command/src/main/java/com/lenis0012/pluginutils/command/CommandModule.java
@@ -1,6 +1,5 @@
 package com.lenis0012.pluginutils.command;
 
-import com.destroystokyo.paper.event.server.AsyncTabCompleteEvent;
 import com.lenis0012.pluginutils.modules.ModularPlugin;
 import com.lenis0012.pluginutils.modules.Module;
 
