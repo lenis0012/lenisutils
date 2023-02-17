288dd0e838b7749906a3deb84d99ae4cf00fe8b7
Lennart ten Wolde <lenny.devmail@gmail.com>
2023-02-17 12:24:18 +0100

    fix: compile

--- release.sh
+++ release.sh
@@ -3,3 +3,7 @@
 npx @dwmkerr/standard-version --packageFiles pom.xml --bumpFiles pom.xml
 git push --follow-tags origin master
 
+echo "Waiting 30 seconds before bumping dev version"
+sleep 30
+
+mvn versions:set -DnewSnapshotVersion=true -DgenerateBackupPoms=false
