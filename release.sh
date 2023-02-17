49ccf45fb578bbc7a6baaf4c5c9b981d294b40bc
Lennart ten Wolde <lenny.devmail@gmail.com>
2023-02-17 12:39:00 +0100

    chore: update release script

--- release.sh
+++ release.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-npx @dwmkerr/standard-version --packageFiles pom.xml --bumpFiles pom.xml
-git push --follow-tags origin master
+CURRENT_VERSION=$(./git-semver latest)
+NEXT_VERSION=$(./git-semver next)
+echo "Getting ready to update from v$CURRENT_VERSION to v$NEXT_VERSION"
+echo $(./git-semver log)
 
-echo "Waiting 30 seconds before bumping dev version"
-sleep 30
+# Ask for confirmation
+read -p "Are you sure you want to release v$NEXT_VERSION? (y/n) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
 
-mvn versions:set -DnewSnapshotVersion=true -DgenerateBackupPoms=false
+CHANGES=$(./git-semver log --markdown)
+echo "TEST"
+
+#npx @dwmkerr/standard-version --packageFiles pom.xml --bumpFiles pom.xml
+#git push --follow-tags origin master
+#
+#echo "Waiting 30 seconds before bumping dev version"
+#sleep 30
+#
+#mvn versions:set -DnewSnapshotVersion=true -DgenerateBackupPoms=false
